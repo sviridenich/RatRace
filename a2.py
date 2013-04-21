0ce3ec91d2e2e88b8a27c8748659b7151e82a074
Sergey Sviridenko <Sergey.Sviridenko@gmail.com>
2013-04-21 11:03:42 +0300

    I wrote methods for classes Rat and Maze

--- a2.py
+++ a2.py
@@ -36,9 +36,98 @@ RAT_2_CHAR = 'P'
 class Rat:
     """ A rat caught in a maze. """
 
-    # Write your Rat methods here.
+    def __init__(self, symbol,  row, col):
+        self.symbol = symbol
+        self.row = row
+        self.col = col
+        self.num_sprouts_eaten = 0
 
+    def __str__(self):
+        res = "Rat {0} at {1} ate {2} sprouts".format( self.symbol, (self.row,self.col) , self.num_sprouts_eaten ) 
+        return res
+
+    def set_location(self, row, col):
+        self.row = row
+        self.col = col
+
+    def get_location(self):
+        return (self.row, self.col)
+
+    def eat_sprout(self):
+        self.num_sprouts_eaten += 1
+    
 class Maze:
     """ A 2D maze. """
 
-    # Write your Maze methods here.
+    def __init__(self, maze_str, rat_1, rat_2 ):
+        self.maze = maze_str
+        self.rat_1 = rat_1
+        self.rat_2 = rat_2
+        self.num_sprouts_left = 0
+
+    def __str__(self):
+        res = ""
+        rows = len(self.maze)
+        cols = len(self.maze[0])
+        for i in range(rows):
+            for j in range(cols):
+                char = self.get_character( i, j )
+                res += char
+            res += "\n"
+        res += str(self.rat_1)
+        res += "\n"
+        res += str(self.rat_2)
+        return res
+                
+    def is_wall(self, row, col):
+        elem = self.maze[row][col]
+        return ( elem is WALL )
+
+    def get_character(self, row, col ):
+        elem = self.maze[row][col]
+        if self.rat_1.get_location() == ( row, col ):
+            elem = self.rat_1.symbol
+        if self.rat_2.get_location() == ( row, col ):
+            elem = self.rat_2.symbol
+        return elem
+
+    def set_character(self, row, col, new_char ):
+        self.maze[row][col] = new_char
+
+    def move(self, Rat, vert_change, hor_change ):
+        new_location = ( Rat.row + vert_change, Rat.col + hor_change )
+        elem = self.get_character( new_location[0], new_location[1] )
+        if elem is WALL:
+            return False
+        if elem is HALL:
+            Rat.set_location(new_location[0], new_location[1] )
+            if elem is SPROUT:
+                Rat.eat_sprout()
+                self.set_character( new_location[0], new_location[1], HALL)
+            return True
+            
+    
+if __name__ == "__main__":
+
+    Jen = Rat(RAT_1_CHAR,1,4)
+    print(Jen)
+    
+    Jen.set_location(2,5)
+    Jen.eat_sprout()
+    print(Jen)
+
+    Paul = Rat(RAT_2_CHAR, 3, 5 )
+
+    Doom = Maze([['#', '#', '#', '#', '#', '#', '#'], 
+      ['#', '.', '.', '.', '.', '.', '#'], 
+      ['#', '.', '#', '#', '#', '.', '#'], 
+      ['#', '.', '.', '@', '#', '.', '#'], 
+      ['#', '@', '#', '.', '@', '.', '#'], 
+      ['#', '#', '#', '#', '#', '#', '#']], 
+      Jen,
+      Paul)
+
+    print(Doom)
+    
+
+    
